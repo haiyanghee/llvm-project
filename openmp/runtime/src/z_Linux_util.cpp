1c059ded7aff6c53b04a845e3591a8da08bce279
Haiyang He <Haiyang_He@mentor.com>
2025-07-24 12:23:27 -0600

    Prevented double atfork() handler initialization

--- openmp/runtime/src/z_Linux_util.cpp
+++ openmp/runtime/src/z_Linux_util.cpp
@@ -1316,11 +1316,13 @@ static void __kmp_atfork_child(void) {
 }
 
 void __kmp_register_atfork(void) {
-  if (__kmp_need_register_atfork) {
+  // NOTE: we will not double register our fork handlers! It will cause deadlock
+  if (!__kmp_already_registered_atfork && __kmp_need_register_atfork) {
     int status = pthread_atfork(__kmp_atfork_prepare, __kmp_atfork_parent,
                                 __kmp_atfork_child);
     KMP_CHECK_SYSFAIL("pthread_atfork", status);
     __kmp_need_register_atfork = FALSE;
+    __kmp_already_registered_atfork = TRUE;
   }
 }
 
